a9e0055214fb32ebaa4ba815b265332c5b108edf
ricardosilvafernandesbr <ricardosilvafernandesbr@gmail.com>
2023-11-12 09:39:30 -0300

    Inicio do sidebar

--- src/pages/private/Home/index.tsx
+++ src/pages/private/Home/index.tsx
@@ -4,7 +4,16 @@ export function Home () {
   return (
     <div className={styles.background}>
       <main className={styles.container}>
-        <aside></aside>
+        <aside className={styles.sidebar}> 
+              <button>pRescrição</button>
+              <button>Paciente</button>                
+              <button>Resumo de alta</button>
+              <button>aDmissão</button>                
+              <button>Exames</button>
+              <button>Evolução Diária</button>                
+              <button>evoLução Noturna</button>
+              <button>enferMagem</button>                
+        </aside>
 
         <div className={styles.content}>
           <header className={styles.header}>
