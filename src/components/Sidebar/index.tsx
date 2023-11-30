a92149410f2a1f13f84a54163b83b254888a1e9c
ricardosilvafernandesbr <ricardosilvafernandesbr@gmail.com>
2023-11-30 19:03:13 -0300

    Criando icones para sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import {Syringe, Stethoscope, Exam, PencilLine, AddressBook, Person, Prescription } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 
 import styles from "./sidebar.module.css";
@@ -5,14 +6,14 @@ import styles from "./sidebar.module.css";
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <Link to="/CadPacientes">Pacientes</Link>
-      <Link to="/Prescricoes">Prescrição UTI</Link>
-      <Link to="/Resumoalta">Resumo de Alta</Link>
-      <Link to="/Admissao">Admissão</Link>
-      <Link to="/Exames">Exames</Link>
-      <Link to="/Evolucaodiaria">Evolução Diária</Link>
-      <Link to="/Evolucaonoturna">Evolução Noturna</Link>
-      <Link to="/Evolucaoenfermagem">Evolução Enfermagem</Link>
+      <Link to="/CadPacientes">Pacientes <Person size={17} /></Link>            
+      <Link to="/Prescricoes">Prescrição UTI <Prescription size={17}/></Link>
+      <Link to="/Resumoalta">Resumo de Alta <AddressBook size={17} /></Link>
+      <Link to="/Admissao">Admissão <PencilLine size={17} /></Link>
+      <Link to="/Exames">Exames <Exam size={17} /></Link>
+      <Link to="/Evolucaodiaria">Evolução Diária <Stethoscope size={17} /></Link>
+      <Link to="/Evolucaonoturna">Evolução Noturna <Stethoscope size={17}/> </Link>
+      <Link to="/Evolucaoenfermagem">Evolução Enfermagem <Syringe size={17} /></Link>
     </aside>
   );
 }
